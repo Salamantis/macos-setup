55ae7feb638d98d689dd57348d111e907a45f562
Salamantis <matthewj.kennedy@icloud.com>
2021-02-09 10:59:02 +1100

    added wait commands

--- install.sh
+++ install.sh
@@ -57,38 +57,42 @@ echo "Your selection is $OPTION"
 #create the temp dir to put all the install artifacts
 mkdir artifacts
 cd artifacts
-#install xcode cli tools to enable install of most other tools
-xcode-select --install
-
-echo -n '- Installing brew ... '
-echo | /usr/bin/ruby -e "$(curl -fsSL $URL_BREW)" > /dev/null
-if [ $? -eq 0 ]; then echo 'OK'; else echo 'NG'; fi
-
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+wait
 #####brew packages to install
 #safe tools
 if [[ $OPTION != 0 ]] ; then
 	brew install ${SAFE_PACKAGES[@]}
+	wait
 fi
 if [[ $OPTION != 0 ]] || [[ $OPTION != 1 ]] ; then
 	brew install ${CASK_PACKAGES[@]} --cask
 	#Mac App Store installs (mas) 
 	mas install ${MAS_PACKAGES[@]}
+	wait
 fi
 #unsafe tools for secwork
 if [[ $OPTION = 3 ]] ; then
 	brew install ${UNSAFE_PACKAGES[@]}
+	wait
 fi
 ####VIM####
 #download plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+wait
 #copy my vimrc file
 cp vimrc ~/.vimrc
 #install plugins so they are ready to go 
+wait
 vim -c 'PlugInstall | qa'
 ######get ohmyzsh
+wait
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+wait
 chmod 700 install.sh
 ./install.sh
 #install powerline
+wait
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cp zshrc ~/.zshrc
